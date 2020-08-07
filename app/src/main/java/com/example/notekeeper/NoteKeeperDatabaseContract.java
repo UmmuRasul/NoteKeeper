2e8b251ae7b2f4ab7770f7f9729825b4282fc013
UmmuRasul <ummurasul5@gmail.com>
2020-08-08 01:23:03 +0300

    Database Contrct class modification

--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -2,8 +2,8 @@ package com.example.notekeeper;
 
 import android.provider.BaseColumns;
 
-public final class NoteKeeperDatabaseContact {
-    private NoteKeeperDatabaseContact(){}
+public final class NoteKeeperDatabaseContract {
+    private NoteKeeperDatabaseContract(){}
 
     public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
@@ -23,14 +23,14 @@ public final class NoteKeeperDatabaseContact {
         public  static  final String TABLE_NAME = "note_info";
         public  static  final String COLUMN_NOTE_TITLE = "note_title";
         public  static  final String COLUMN_NOTE_TEXT = "note_text";
-        public  static  final String COLUMN_COURSE__ID = "course_id";
+        public  static  final String COLUMN_COURSE_ID = "course_id";
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE" + TABLE_NAME + "(" +
                         _ID + "INTEGER PRIMARY KEY, " +
                         COLUMN_NOTE_TITLE + " TEXT  NOT NULL," +
                         COLUMN_NOTE_TEXT + "TEXT," +
-                        COLUMN_COURSE__ID +"TEXT  NOT NULL)";
+                        COLUMN_COURSE_ID +"TEXT  NOT NULL)";
 
     }
 }
